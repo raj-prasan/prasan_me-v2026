64fceaa9c1222b4466d27602f9ad2b596c32a6e9
raj-prasan <rajprasanj@gmail.com>
2026-05-09 22:58:57 +0530

    sorting of posts in PostsPage component and fixed next.js static render on posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,33 +1,41 @@
+export const dynamic = "force-dynamic";
 import { ShellSection } from "@/components/ui/shell";
 import Post from "./post";
 import { prisma } from "@/lib/prisma";
 
-const PostsPage = async() => {
+const PostsPage = async () => {
   const postData = await prisma.post.findMany();
-  return ( 
+  return (
     <div>
       <section className="prose prose-zinc dark:prose-invert text-[15px] animate-slide-from-down-and-fade-2 text-pretty items-center gap-8 container py-3 md:py-4 space-y-12">
         <p className="leading relaxed">
-          I built this page to capture and share small updates, ideas, and progress—without overthinking or waiting for something big.
+          I built this page to capture and share small updates, ideas, and
+          progress—without overthinking or waiting for something big.
         </p>
       </section>
       <ShellSection index={2} title="POSTS" foreground>
         <div className="flex flex-col gap-6">
-          {postData.map((post) => (
-            <Post
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              content={post.content}
-              image={post.image ?? undefined}
-              createdAt={post.createdAt.toISOString()}
-              tags={post.tags}
-            />
-          ))}
+          {postData
+            .sort(
+              (a, b) =>
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime(),
+            )
+            .map((post) => (
+              <Post
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                content={post.content}
+                image={post.image ?? undefined}
+                createdAt={post.createdAt.toISOString()}
+                tags={post.tags}
+              />
+            ))}
         </div>
       </ShellSection>
     </div>
-   );
-}
- 
+  );
+};
+
 export default PostsPage;
